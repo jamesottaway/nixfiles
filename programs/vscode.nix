ddf436bf00092d2125a08d7fdb00fba6aa2fbf01
James Ottaway <git@james.b12y.com>
2020-06-21 12:34:31 +1000

    More VSCode extensions

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -2,6 +2,16 @@
 
 let
   inherit (pkgs) vscode-extensions;
+  inherit (pkgs.vscode-utils) buildVscodeMarketplaceExtension;
+
+  arrterian.nix-env-selector = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "nix-env-selector";
+      publisher = "arrterian";
+      version = "0.1.2";
+      sha256 = "1n5ilw1k29km9b0yzfd32m8gvwa2xhh6156d4dys6l8sbfpp2cv9";
+    };
+  };
 in
 
 {
@@ -10,7 +20,11 @@ in
   programs.vscode = {
     enable = true;
     extensions = with vscode-extensions; [
+      arrterian.nix-env-selector
+      bbenoist.Nix
       ms-vscode-remote.remote-ssh
+      redhat.vscode-yaml
+      vscodevim.vim
     ];
   };
 }
