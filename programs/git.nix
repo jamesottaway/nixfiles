db30d3cd8e1df2d928f062a1d28c6dfdedb28257
James Ottaway <git@james.b12y.com>
2020-08-06 20:55:50 +1000

    Tweak Git aliases and config

--- programs/git.nix
+++ programs/git.nix
@@ -6,15 +6,18 @@
     userName = "James Ottaway";
     aliases = {
       a = "add";
+      amend = "commit --amend";
       cm = "commit --message";
       co = "checkout";
       d = "diff";
       ds = "diff --staged";
+      fixup = "commit --fixup";
       l = "log --graph --oneline";
-      s = "status --short";
+      r = "restore";
     };
     extraConfig = {
       checkout.defaultRemote = "origin";
+      init.defaultBranch = "main";
       merge.conflictstyle = "diff3";
       push.default = "current";
       rebase.autosquash = true;
