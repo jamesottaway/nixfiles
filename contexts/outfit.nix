5787ca2ccce31af3b2927c4f81e2965cd9c25fff
James Ottaway <james.ottaway@outfit.io>
2020-10-14 09:35:59 +1000

    Need `saml2aws` at work

--- contexts/outfit.nix
+++ contexts/outfit.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ../programs/jira.nix
@@ -6,4 +8,8 @@
   programs.git.userEmail = "james.ottaway@outfit.io";
 
   programs.zsh.cdpath = ["~/src/github.com/Outfitio"];
+
+  home.packages = [
+    pkgs.saml2aws
+  ];
 }
