67cf9ea0e7445439c07c901faea9f370ee7c6237
James Ottaway <james.ottaway@outfit.io>
2021-01-05 09:33:12 +1100

    Simplify my Yabai config while I work out this second monitor

--- services/yabai.nix
+++ services/yabai.nix
@@ -6,7 +6,6 @@
     package = pkgs.yabai;
     config = {
       layout = "bsp";
-      focus_follows_mouse = "autofocus";
     };
     extraConfig = ''
       yabai -m rule --add app='System Preferences' manage=off
@@ -14,8 +13,6 @@
       yabai -m rule --add app='Firefox' title='Picture-in-Picture' manage=off
       yabai -m rule --add app='Wally' manage=off
       yabai -m rule --add app='1Password' title='Preferences' manage=off
-      yabai -m space --padding abs:10:10:10:10
-      yabai -m space --gap abs:10
     '';
   };
 }
