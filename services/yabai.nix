af79ee338b637b66a4beaa2af617376047fa05c3
James Ottaway <james.ottaway@outfit.io>
2021-01-06 09:56:27 +1000

    More preference pane exceptions to Yabai's tiling

--- services/yabai.nix
+++ services/yabai.nix
@@ -13,6 +13,9 @@
       yabai -m rule --add app='Firefox' title='Picture-in-Picture' manage=off
       yabai -m rule --add app='Wally' manage=off
       yabai -m rule --add app='1Password' title='Preferences' manage=off
+      yabai -m rule --add app='MeetingBar' title='MeetingBar Preferences' manage=off
+      yabai -m rule --add app='Docker Desktop' title='Settings' manage=off
+      yabai -m rule --add app='Spotify' manage=off
     '';
   };
 }
