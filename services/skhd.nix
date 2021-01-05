73761a7536e5e128c61d461439b8a27a0ef37858
James Ottaway <james.ottaway@outfit.io>
2021-01-06 09:56:03 +1000

    Drive more yabai magic from skhd shortcuts

--- services/skhd.nix
+++ services/skhd.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 
+let
+  yabai = "${pkgs.yabai}/bin/yabai";
+in
+
 {
   services.skhd = {
     enable = true;
@@ -8,6 +12,12 @@
       meh - s : open /Applications/Slack.app
       meh - f : open /Applications/Firefox.app
       meh - c : open "${pkgs.vscode}/Applications/Visual Studio Code.app"
+      meh - z : ${yabai} -m window --toggle zoom-fullscreen
+      meh - q : ${yabai} -m window --display recent && ${yabai} -m display --focus recent
+      meh - left : ${yabai} -m window --swap west
+      meh - down : ${yabai} -m window --swap south
+      meh - up : ${yabai} -m window --swap north
+      meh - right : ${yabai} -m window --swap east
     '';
   };
 }
