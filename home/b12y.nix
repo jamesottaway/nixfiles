15711278552dd26796f4a41f30d1261ba79d3281
James Ottaway <james.ottaway@hireup.com.au>
2021-01-26 22:22:07 +1000

    Need to SSH to my homelab machines

--- home/b12y.nix
+++ home/b12y.nix
@@ -1,5 +1,10 @@
+{ pkgs, ... }:
+
 {
   imports = [ ./. ];
   programs.git.userEmail = "git@james.b12y.com";
   programs.zsh.cdpath = ["~/src/github.com/jamesottaway"];
+  programs.vscode.extensions = with pkgs.vscode-extensions; [
+    ms-vscode-remote.remote-ssh
+  ];
 }
