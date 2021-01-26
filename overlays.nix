38956ed38bd0b016f159d97b05fe3dcda2bd061a
James Ottaway <james.ottaway@hireup.com.au>
2021-01-26 21:00:51 +1000

    Remove old overlay-based packages

--- overlays.nix
+++
@@ -1,9 +0,0 @@
-[
-  (
-    self: super: {
-      tmux-up = super.callPackage ./packages/tmux-up.nix {};
-      git-filter-repo = super.callPackage ./packages/git-filter-repo.nix {};
-      wally-cli = super.callPackage ./packages/wally-cli.nix { wally-cli = super.wally-cli; };
-    }
-  )
-]
