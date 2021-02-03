ad5ee6bd4d7220757fea125906efd233df30b96d
James Ottaway <james.ottaway@hireup.com.au>
2021-02-03 11:50:53 +1000

    Enable the experimental `ca-references` feature

--- darwin/nix.nix
+++ darwin/nix.nix
@@ -4,6 +4,6 @@
   services.nix-daemon.enable = true;
   nix.package = pkgs.nixFlakes;
   nix.extraOptions = ''
-    experimental-features = nix-command flakes
+    experimental-features = nix-command flakes ca-references
   '';
 }
