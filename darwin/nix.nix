52c4ab606c353c13774a98135d8e2c453464a2db
James Ottaway <james.ottaway@hireup.com.au>
2021-03-12 15:56:33 +1000

    Pin nixpkgs in the system registry

--- darwin/nix.nix
+++ darwin/nix.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   services.nix-daemon.enable = true;
   nix.package = pkgs.nixFlakes;
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix.extraOptions = ''
     experimental-features = nix-command flakes ca-references
   '';
