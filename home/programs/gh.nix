00d226a1ee5a1b089346dcac6cda8041bfc8c2ac
James Ottaway <git@james.b12y.com>
2021-04-14 22:41:39 +1000

    gh completion --shell zsh

--- home/programs/gh.nix
+++ home/programs/gh.nix
@@ -1,6 +1,20 @@
+{ pkgs, ... }:
+
+let
+  completion = pkgs.runCommand "_gh" {
+    buildInputs = [ pkgs.gh ];
+  } ''
+    gh completion --shell zsh > $out
+  '';
+in
+
 {
   programs.gh = {
     enable = true;
     gitProtocol = "ssh";
   };
+
+  programs.zsh.initExtra = ''
+    fpath+=(${completion})
+  '';
 }
