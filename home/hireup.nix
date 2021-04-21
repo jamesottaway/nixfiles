9e40baa36a87d27cdbd3f1c87e531bb80741b539
James Ottaway <james.ottaway@hireup.com.au>
2021-04-21 13:25:18 +1000

    Try the new MongoSH shell

--- home/hireup.nix
+++ home/hireup.nix
@@ -5,4 +5,5 @@
   home.packages = [ pkgs.armada.defaultPackage ];
   programs.git.userEmail = "james.ottaway@hireup.com.au";
   programs.zsh.cdpath = ["~/src/github.com/hireupau"];
+  home.file.".mongoshrc.js".source = ./misc/mongoshrc.js;
 }
---
+++ home/misc/mongoshrc.js
@@ -0,0 +1,1 @@
+const oid = ObjectId;
