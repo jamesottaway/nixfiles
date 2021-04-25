599ecf289148c3112b623e715db313b1e8e72d93
James Ottaway <git@james.b12y.com>
2021-04-25 20:39:46 +1000

    Tidy up `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,47 +1,56 @@
 {
-  description = "$HOME/.dotfiles";
-
   inputs = {
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    darwin.url = github:jamesottaway/nix-darwin/fix/nix-flake-info;
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = github:nix-community/home-manager;
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+
+    darwin = {
+      url = github:jamesottaway/nix-darwin/fix/nix-flake-info;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     armada = {
       url = "git+ssh://git@github.com/hireupau/armada?ref=nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, home-manager, nixpkgs, darwin, armada, ... }@inputs: {
-    darwinConfigurations.b12y = darwin.lib.darwinSystem {
-      modules = [
-        ./darwin/default.nix
-        ./darwin/b12y.nix
-        ./home/manager.nix
-        home-manager.darwinModule
-        { home-manager.users.root = ./home/root.nix; }
-        { home-manager.users.james = ./home/b12y.nix; }
-      ];
+  outputs = { self, nixpkgs, darwin, home-manager, armada, ... }: {
+    lib = {
+      hireup = {
+        homeManagerConfiguration = import ./hireup/home-manager;
+        darwinConfiguration = import ./hireup/nix-darwin;
+      };
     };
 
-    darwinConfigurations.hireup = darwin.lib.darwinSystem {
-      modules = [
-        ./darwin/default.nix
-        ./darwin/hireup.nix
-        ./home/manager.nix
-        self.hireup.darwinConfiguration
-        home-manager.darwinModule
-        { nixpkgs.overlays = [ armada.overlay ]; }
-        { home-manager.users.root = ./home/root.nix; }
-        { home-manager.users.jamesottaway = ./home/hireup.nix; }
-        { home-manager.users.jamesottaway.imports = [ self.hireup.homeManagerConfiguration ]; }
-      ];
-    };
+    darwinConfigurations = {
+      b12y = darwin.lib.darwinSystem {
+        modules = [
+          ./darwin/default.nix
+          ./darwin/b12y.nix
+          ./home/manager.nix
+          home-manager.darwinModule
+          { home-manager.users.root = ./home/root.nix; }
+          { home-manager.users.james = ./home/b12y.nix; }
+        ];
+      };
 
-    hireup = {
-      homeManagerConfiguration = import ./hireup/home-manager;
-      darwinConfiguration = import ./hireup/nix-darwin;
+      hireup = darwin.lib.darwinSystem {
+        modules = [
+          ./darwin/default.nix
+          ./darwin/hireup.nix
+          ./home/manager.nix
+          self.lib.hireup.darwinConfiguration
+          home-manager.darwinModule
+          { nixpkgs.overlays = [ armada.overlay ]; }
+          { home-manager.users.root = ./home/root.nix; }
+          { home-manager.users.jamesottaway = ./home/hireup.nix; }
+          { home-manager.users.jamesottaway.imports = [ self.lib.hireup.homeManagerConfiguration ]; }
+        ];
+      };
     };
 
     checks.x86_64-darwin = {
