96f347bc2ded81c98bc4ee315bdd41d535557488
James Ottaway <git@james.b12y.com>
2021-07-13 21:40:26 +1000

    Don't override HA package

--- homelab/home-assistant/default.nix
+++ homelab/home-assistant/default.nix
@@ -4,19 +4,5 @@
   services.home-assistant = {
     enable = true;
     openFirewall = true;
-    package = pkgs.home-assistant.override {
-      packageOverrides = self: super: {
-        aiogithubapi = self.callPackage ./aiogithubapi.nix { };
-        backoff = self.callPackage ./backoff.nix { };
-      };
-      extraPackages = ps: with ps; [
-        aiogithubapi
-        aiolifx
-        defusedxml
-        netdisco
-        pynacl
-        zeroconf
-      ];
-    };
   };
 }
