77a4fad0de1d34a68f7f888ef2e4d1d703277bf2
James Ottaway <james.ottaway@hireup.com.au>
2022-02-21 10:17:39 +1000

    Fix `error: 'print-dev-env' is not a recognised command`

--- home/programs/direnv.nix
+++ home/programs/direnv.nix
@@ -1,7 +1,10 @@
 {
   programs.direnv = {
     enable = true;
-    nix-direnv.enable = true;
+    nix-direnv = {
+      enable = true;
+      enableFlakes = true;
+    };
     stdlib = builtins.readFile ../misc/direnvrc;
   };
 }
